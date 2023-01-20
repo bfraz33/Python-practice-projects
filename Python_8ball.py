b39adeb27861ae44e8b567d1ce8c439eb28e2867
Brandon Frazier <88755132+bfraz33@users.noreply.github.com>
2023-01-20 16:35:32 -0600

    Update Python_8ball.py

--- Python_8ball.py
+++ Python_8ball.py
@@ -4,10 +4,12 @@ import random
 print("What is your name?")
 name = input()
 print('Hello ' + name)
+#This will prompt the user to input their name and then print their name.
 
 A = ["Of course!", "Without a doubt!", "Unfortunately not", "That is always a possibility",
      "No", "If it's in the cards for you!", "I can see that in your future"]
 Q = True
+#These are the answers that the program will respond with after each question.
 
 while Q:
     quest = input("Please ask me any question (press enter to exit program)")
@@ -17,5 +19,6 @@ while Q:
 
     else:
         print(A[random.randint(0,6)])
+#This randmomizes answers to each question.
 
     quest = input("Please ask another question")
