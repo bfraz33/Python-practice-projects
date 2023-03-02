a59cad7ab0e1ef29412565f4c433c8fcb828ef85
Brandon Frazier <88755132+bfraz33@users.noreply.github.com>
2023-03-02 16:09:53 -0600

    Update Bill_split.py

--- Bill_split.py
+++ Bill_split.py
@@ -1,3 +1,5 @@
+from fpdf import FPDF
+
 class Bill:
     """
     This object contains data about the bill, such as total amount
@@ -33,7 +35,26 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, roommate1, roommate2, bill):
-        pass
+        pdf = FPDF(orientation='P', unit='pt', format="A4")
+        pdf.add_page()
+
+        # Adding image
+        pdf.image("house.png", w=100, h=100)
+
+        # Adding text
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="RENT", border=0, align="C", ln=1)
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        # Name of tenants and amount due for each tenant
+        pdf.cell(w=100, h=40, txt=roommate1.name, border=0)
+        pdf.cell(w=150, h=40, txt=str(round(roommate1.pays(bill, roommate2), 2)), border=0, ln=1)
+
+        pdf.cell(w=100, h=40, txt=roommate2.name, border=0)
+        pdf.cell(w=150, h=40, txt=str(round(roommate2.pays(bill, roommate1), 2)), border=0, ln=1)
+
+        pdf.output(self.filename)
 
 
 the_bill = Bill(amount = 180, period = "February 2023" )
@@ -42,6 +63,10 @@ emily = Roommate(name = "Emily", days_in_house=29)
 
 print("Brandon pays:",brandon.pays(bill=the_bill,roommate2=emily))
 print("Emily pays:", emily.pays(bill=the_bill,roommate2=brandon))
+
+#This generates the PDF
+pdf_report = PdfReport(filename="Report1.pdf")
+pdf_report.generate(roommate1=brandon, roommate2=emily, bill=the_bill)
 
 """"
 The above code will output what each tenant owes based on the amount
