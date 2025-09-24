c465d0804dc8b5587bc4c24ccbf7eee5c4489b61
Brandon Frazier <bfrazier756@gmail.com>
2025-09-23 21:43:18 -0500

    Added scoring

--- python_quiz.py
+++ python_quiz.py
@@ -8,15 +8,22 @@ def show_questions():
     print("There are", len(Questions), "questions in this quiz.")
 
 def Quiz():
+    score = 0
+
     input("Please hit Emter to start:")
     for qnum, qdict in Questions.items():
         for question, answers in qdict.items():
             user_answer = input(f"{qnum}. {question}")
             if user_answer.strip().lower() == answers.strip().lower():
-                print("Correct!✅")          
+                print("Correct!✅")
+                score +=1
             else:
                 print("Wrong❌")
-                
+            score -1
+
+    print(f"You are finished! Your score is", score,
+     "out of 3")
+      
 if __name__=="__main__":
     show_questions()
     Quiz()     
